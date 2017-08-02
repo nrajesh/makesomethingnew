9fd2f484e906a0228b8d5bd53117237e3e195691
Rajesh Narayanan <rajesh.narayanan01@gmail.com>
2017-08-02 18:44:19 +0200

    first commit

---
+++ makesomethingnew/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  makesomethingnew
+//
+//  Created by Rajesh Narayanan on 02/08/2017.
+//  Copyright © 2017 Rajesh Narayanan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
